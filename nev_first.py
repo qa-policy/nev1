e427187d7fbb0ef797865005b51895bd2b0a08ad
qa-policy <qa-policy@yandex.ru>
2019-11-08 13:13:40 +0400

    Add div function

--- nev_first.py
+++ nev_first.py
@@ -1,4 +1,6 @@
 numA = input("Enter the first number: ")
 numB = input("Enter the second number: ")
 num_sum = int(numA) + int(numB)
+num_div =int(numA) / int(numB)
 print("%s + %s = %s" % (numA, numB, str(num_sum)))
+print("%s / %s = %s" % (numA, numB, str(num_div)))
