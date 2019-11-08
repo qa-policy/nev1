9c238712659b24598195c41f7d569e48a61c9198
qa-policy <qa-policy@yandex.ru>
2019-11-08 14:05:45 +0400

    1

--- nev_first.py
+++ nev_first.py
@@ -4,3 +4,6 @@ num_sum = int(numA) + int(numB)
 num_div =int(numA) / int(numB)
 print("%s + %s = %s" % (numA, numB, str(num_sum)))
 print("%s / %s = %s" % (numA, numB, str(num_div)))
+num_mult = int(numA) * int(numB)
+print("%s * %s = %s" % (numA, numB, str(num_mult)))
+
